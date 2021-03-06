aebd904497515f3dcb798abe9d5033662ff7063f
Gabriel Pescaroli <gabnerdcompany@gmail.com>
2021-03-06 10:03:51 -0300

    adicionando a versão mobile da home

---
+++ app/javascript/packs/articles.js
@@ -0,0 +1,7 @@
+toggleCategorias = ()=>{
+    if(document.getElementById("categorias").style.display == "block"){
+        document.getElementById("categorias").style.display ="none";
+    }else{
+        document.getElementById("categorias").style.display ="block";
+    }
+}
